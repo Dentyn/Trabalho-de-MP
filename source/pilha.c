d136d9d6f6f1071c449139de72dc275590099ca0
Dentyn <dentyn_tuey@hotmail.com>
2020-10-17 11:45:52 -0300

    pilha

--- source/pilha.c
+++ source/pilha.c
@@ -31,38 +31,3 @@ int Size(Structure_Stack * Stack)
     return Stack->SizeStack;
 }
 
-void SetSize(Structure_Stack * Stack, int SizeStack)
-{
-    if(SizeStack > EMPTY)
-    {
-        Stack->DataStack = (ItemType *)realloc(Stack->DataStack, SizeStack*sizeof(ItemType));
-        Stack->SizeStack = SizeStack;
-    }
-}
-
-int IsFull(Structure_Stack * Stack)
-{
-    return Top(Stack) == Size(Stack)-1;
-}
-
-int IsEmpty(Structure_Stack * Stack)
-{
-    return Top(Stack) == EMPTY;
-}
-
-Structure_Stack* CreateStack(int SizeStack)
-{
-    if(SizeStack < 0)
-        SizeStack = 0;
-    Structure_Stack* stack = (Structure_Stack *) malloc(sizeof(Structure_Stack));
-    stack->DataStack = (ItemType *) malloc(SizeStack*sizeof(ItemType));
-    stack->TopStack = EMPTY;
-    stack->SizeStack = SizeStack;
-    return stack;
-}
-
-void DestroyStack(Structure_Stack * Stack)
-{
-    free(Stack->DataStack);
-    free(Stack);
-}
